e503da99235c9ddd9a8a4caf9f7792c57c8a1e27
GasbroNinja <gasbroider@live.it>
2023-04-14 16:29:13 +0200

    intialize fetch no redux

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,21 +1,22 @@
-/*
+
 export const ADD_TO_FAVOURITES = "ADD_TO_FAVOURITES";
 export const REMOVE_FROM_FAVOURITES = "REMOVE_FROM_FAVOURITES";
+export const ADD_ALBUM = "ADD_ALBUM";
+/*
 export const ADD_COMPANY_JOBS = "ADD_COMPANY_JOBS";
-export const ADD_JOBS = "ADD_JOBS";
 
 export const addToFavoriteAction = data => ({ type: ADD_TO_FAVOURITES, payload: data });
 export const removeFromFavoriteAction = data => ({ type: REMOVE_FROM_FAVOURITES, payload: data });
+*/
 
- FETCHES
-export const getCompanyAction = params => {
+export const getAlbumsAction = query => {
   return async dispatch => {
-    const baseEndpoint = "https://strive-benchmark.herokuapp.com/api/jobs?company=";
+    const baseEndpoint = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
     try {
-      const response = await fetch(baseEndpoint + params.companyName);
+      const response = await fetch(baseEndpoint + query);
       if (response.ok) {
         const { data } = await response.json();
-        dispatch({ type: ADD_COMPANY_JOBS, payload: data });
+        dispatch({ type: ADD_ALBUM, payload: data });
       } else {
         alert("Error fetching results");
       }
@@ -25,14 +26,17 @@ export const getCompanyAction = params => {
   };
 };
 
-export const getJobsAction = query => {
+
+/*
+ FETCHES
+export const getCompanyAction = params => {
   return async dispatch => {
-    const baseEndpoint = "https://strive-benchmark.herokuapp.com/api/jobs?search=";
+    const baseEndpoint = "https://strive-benchmark.herokuapp.com/api/jobs?company=";
     try {
-      const response = await fetch(baseEndpoint + query + "&limit=20");
+      const response = await fetch(baseEndpoint + params.companyName);
       if (response.ok) {
         const { data } = await response.json();
-        dispatch({ type: ADD_JOBS, payload: data });
+        dispatch({ type: ADD_COMPANY_JOBS, payload: data });
       } else {
         alert("Error fetching results");
       }
