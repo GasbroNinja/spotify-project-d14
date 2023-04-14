aad3aa3bd54ff20891859f947251f771b81b0786
GasbroNinja <gasbroider@live.it>
2023-04-14 16:40:19 +0200

    Homepage with redux

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -37,7 +37,9 @@ const Album = () => {
               <div className="col-md-3 pt-5 text-center" id="img-container" />
               <div className="col-md-8 p-5">
                 <div className="row">
-                  <div className="col-md-10 mb-5" id="trackList" />
+                  <div className="col-md-10 mb-5" id="trackList">
+                    
+                  </div>
                 </div>
               </div>
             </div>
